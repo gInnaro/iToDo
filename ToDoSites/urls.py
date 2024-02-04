981076579b5b618cf656de42a112027e0345892e
gInnaro <ilnaz.gan@gmail.com>
2024-02-04 14:14:07 +0300

    remove note

--- ToDoSites/urls.py
+++ ToDoSites/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('chat/', include('gpt.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('token/', obtain_auth_token),
     path('profile/', include('user_profile.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
